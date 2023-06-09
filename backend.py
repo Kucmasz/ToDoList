9250ead59014a97dae0123397cf2bfdad535441e
xTrurl <59177982+xTrurl@users.noreply.github.com>
2023-06-09 21:28:54 +0200

    Basic user management

--- backend.py
+++ backend.py
@@ -1,23 +1,95 @@
 from flask import Flask, jsonify, request, make_response
+import secrets
 
 app = Flask(__name__)
 
-tasks = []
+
+class User:
+    def __init__(self, username):
+        self.username = username
+        self.tasks = []
+        self.current_session_id = ""
 
 
+users = []
+active_user = None
+
+
+def find_user_by_username(username):
+    for user in users:
+        if user.username == username:
+            return user
+    return None
+
+# :*
 @app.route('/tasks', methods=['POST', 'GET', 'DELETE'])
 def handle_tasks():
+    if active_user is not None:
+        if request.method == 'POST':
+            data = request.get_json()
+            active_user.tasks.append(data['task'])
+            return jsonify({'message': 'Task added successfully'}), 201
+        elif request.method == 'GET':
+            return jsonify(active_user.tasks), 200
+        elif request.method == 'DELETE':
+            active_user.tasks.clear()
+            return jsonify({'message': 'Task deleted'}), 200
+    else:
+        return jsonify({'message': 'Task deleted'}), 401
+
+@app.route('/users/register', methods=['POST', 'DELETE'])
+def handle_account():
+    username = request.json.get('username')
+    user = find_user_by_username(username)
     if request.method == 'POST':
-        data = request.get_json()
-        tasks.append(data['task'])
-        return jsonify({'message': 'Task added successfully'})
-    elif request.method == 'GET':
-        return jsonify(tasks)
+        if not user:
+            users.append(User(username))
+            return jsonify({'message': 'User created'}), 201
+        else:
+            return jsonify({'message': 'User already exists'}), 409
     elif request.method == 'DELETE':
-        tasks.clear()
-        response = make_response('')
-        response.status_code = 200
-        return response
+        if user:
+            users.remove(user)
+            return jsonify({'message': 'User successfully deleted'}), 204
+        else:
+            return jsonify({'message': 'User not found'}), 400
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
+
+
+# Endpoint for user login
+@app.route('/users/login', methods=['POST'])
+def login():
+    global active_user
+    username = request.json.get('username')
+    user = find_user_by_username(username)
+
+    if user:
+        session_id = secrets.token_hex(16)
+        user.current_session_id = session_id
+        active_user = user
+        return jsonify({'message': 'Logged in successfully', 'session_id': session_id}), 200
+    else:
+        return jsonify({'message': 'User not found'}), 401
+
+
+# Endpoint for user logout
+@app.route('/users/logout', methods=['POST'])
+def logout():
+    global active_user
+    username = request.json.get('username')
+    user = find_user_by_username(username)
+
+    if user and user == active_user:
+        session_id = request.json.get('session_id')
+        if session_id:
+            user.current_session_id = 0
+            active_user = None
+            return jsonify({'message': 'Logged out successfully'}), 200
+        else:
+            return jsonify({'message': 'Invalid session ID'}), 401
+    else:
+        return jsonify({'message': 'Invalid user'}), 401
 
 
 if __name__ == '__main__':
--- frontend.py
+++ frontend.py
@@ -1,9 +1,45 @@
 import requests
 
+session_id = ''
+
+def create_user(username):
+    response = requests.post('http://localhost:5000/users/register', json={'username': username})
+    if response.status_code == 201:
+        print('Registration successful')
+    else:
+        print('Registration failed')
+
+
+def delete_user(username):
+    response = requests.delete('http://localhost:5000/users/register', json={'username': username})
+    if response.status_code == 200:
+        print('User account deleted successfully.')
+    else:
+        print('User acount not deleted.')
+
+
+def login_user(username):
+    global session_id
+    response = requests.post('http://localhost:5000/users/login', json={'username': username})
+    if response.status_code == 200:
+        print('Login successful.')
+        session_id = response.json()['session_id']
+    else:
+        print('Login failed.')
+
+
+def logout_user(username):
+    global session_id
+    response = requests.post('http://localhost:5000/users/logout', json={'username': username, 'session_id': session_id})
+    if response.status_code == 200:
+        print('Logout successful.')
+    else:
+        print('Logout failed.')
+
 
 def add_task(task):
     response = requests.post('http://localhost:5000/tasks', json={'task': task})
-    if response.status_code == 200:
+    if response.status_code == 201:
         print('Task added successfully')
     else:
         print('Failed to add the task')
@@ -19,6 +55,7 @@ def get_tasks():
     else:
         print('Failed to retrieve tasks')
 
+
 def delete_tasks():
     response = requests.delete('http://localhost:5000/tasks')
     if response.status_code == 200:
@@ -29,7 +66,11 @@ def delete_tasks():
 
 def main():
     print("Usage:")
-    print(" - add \"task\" - create a new task")
+    print(" - register <user> - create a user")
+    print(" - delete <user> - delete a user")
+    print(" - login <user> - log in as a user")
+    print(" - logout - logout currently logged in user")
+    print(" - add <task> - create a new task")
     print(" - get - print the list of tasks")
     print(" - delete - clear all tasks")
     while True:
@@ -40,6 +81,18 @@ def main():
         command = words[0]
         words.remove(command)
 
+        if command == "register":
+            username = ' '.join(words)
+            create_user(username)
+        if command == "delete":
+            username = ' '.join(words)
+            delete_user(username)
+        if command == "login":
+            username = ' '.join(words)
+            login_user(username)
+        if command == "logout":
+            username = ' '.join(words)
+            logout_user(username)
         if command == "add":
             task = ' '.join(words)
             add_task(task)
